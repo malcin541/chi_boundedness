e0b9f79de451368522e5e3049be1bfb40b806687
Marcin Pilipczuk <m.pilipczuk@uw.edu.pl>
2026-02-04 20:52:30 +0100

    work on degeneracy

--- ChiBoundedness/Degeneracy.lean
+++ ChiBoundedness/Degeneracy.lean
@@ -62,4 +62,42 @@ theorem degeneracy_def_equivalent (G : SimpleGraph V) (d : ℕ) :
       constructor
       · exact H.edge_vert hx'.symm
       · exact H.adj_sub hx'.symm
+
+theorem empty_zero_degenerate (G : SimpleGraph V) (d : ℕ) : G = ⊥ → IsDegenerate G d := by
+  intro h H h_H_not_bot
+  obtain ⟨ v, hv ⟩ := H.ne_bot_iff_nonempty_verts.mp h_H_not_bot
+  use v
+  constructor
+  · exact hv
+  · suffices h₁ : (H.neighborSet v) = ∅ by
+      unfold SimpleGraph.Subgraph.degree
+      rw [← Set.toFinset_card, ← Set.ncard_eq_toFinset_card']
+      apply (H.neighborSet v).ncard_eq_zero.mpr at h₁
+      simp [h₁]
+    unfold SimpleGraph.Subgraph.neighborSet
+    ext x
+    simp only [Set.mem_setOf_eq, Set.mem_empty_iff_false, iff_false]
+    suffices h₂ : ¬G.Adj v x by
+      exact mt H.adj_sub h₂
+    simp [h]
+
+theorem mon_degeneracy (G : SimpleGraph V) (d₁ d₂ : ℕ) :
+    IsDegenerate G d₁ → d₁ ≤ d₂ → IsDegenerate G d₂ := by
+  intro h₁ h_ineq H h_H_neq_bot
+  obtain ⟨ v, hv ⟩ := h₁ H h_H_neq_bot
+  exact ⟨ v, ⟨ hv.left, le_trans hv.right h_ineq ⟩ ⟩
+
+theorem degeneracy_le_maxDegree (G : SimpleGraph V) : IsDegenerate G G.maxDegree := by
+  intro H h_H_neq_bot
+  obtain ⟨ v, hv ⟩ := H.ne_bot_iff_nonempty_verts.mp h_H_neq_bot
+  exact ⟨ v, ⟨ hv, le_trans (H.degree_le v) (G.degree_le_maxDegree v) ⟩ ⟩
+
+theorem degeneracy_subgraph_monotone (G : SimpleGraph V) (H : G.Subgraph) (d : ℕ) :
+    IsDegenerate G d → IsDegenerate H.coe d := by
+  intro h K h_K_neq_bot
+  obtain K' := H.coeSubgraph K
+  have h_K'_neq_bot : K' ≠ ⊥ := by sorry
+  obtain ⟨ v, hv ⟩ := h K' h_K'_neq_bot
+  sorry
+
 end Degeneracy
