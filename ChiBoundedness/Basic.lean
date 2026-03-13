30ddffeb79353856e6eeb0ff030109e6b0579b34
Marcin Pilipczuk <m.pilipczuk@uw.edu.pl>
2026-03-13 10:21:16 +0100

    Fintype V -> Finite V

--- ChiBoundedness/Basic.lean
+++ ChiBoundedness/Basic.lean
@@ -6,26 +6,26 @@ import Mathlib
 
 open Finset Fintype
 
-variable {V : Type*} [Fintype V] {G : SimpleGraph V}
-
 set_option diagnostics true
 
 /- Some unimportant stuff needed because SimpleGraph can be infinite -/
-lemma finite_graph_chromaticNumber_ne_top :
+lemma finite_graph_chromaticNumber_ne_top {V : Type*} [Finite V] (G : SimpleGraph V) :
     G.chromaticNumber ≠ ⊤ := by
   rw [G.chromaticNumber_ne_top_iff_exists]
+  haveI := Fintype.ofFinite V
   use Fintype.card V
   exact G.colorable_of_fintype
 
-lemma size_le_chromaticNumber_times_indepNumber :
-    Fintype.card V ≤ G.chromaticNumber.toNat * G.indepNum := by
+lemma size_le_chromaticNumber_times_indepNumber {V : Type*} [Finite V] (G : SimpleGraph V) :
+    Nat.card V ≤ G.chromaticNumber.toNat * G.indepNum := by
   /- Define optimum set of colors and coloring -/
   set opt_coloring : G.Coloring (Fin G.chromaticNumber.toNat) :=
     (G.colorable_chromaticNumber_of_fintype).toColoring (by simp)
   have h_opt_colors_eq_chi : G.chromaticNumber = Fintype.card (Fin G.chromaticNumber.toNat) := by
     simp [ENat.coe_toNat, finite_graph_chromaticNumber_ne_top]
   /- Apply the crucial cardinality multiplication lemma -/
-  rw [h_opt_colors_eq_chi, ENat.toNat_coe, Nat.mul_comm]
+  haveI := Fintype.ofFinite V
+  rw [Nat.card_eq_fintype_card, h_opt_colors_eq_chi, ENat.toNat_coe, Nat.mul_comm]
   refine Finset.card_le_mul_card_image_of_maps_to (f := opt_coloring) (by simp) G.indepNum ?_
   /- Conclude that a color class has cardinality at most G.indepNum.
      This should be easy, but there is some tricky typecasting between Finset and Set V -/
